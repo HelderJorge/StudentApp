ea1ba9ffbdee80609f6061e586e3f68e3295db93
HelderJorge <48417951+HelderJorge@users.noreply.github.com>
2019-03-24 15:35:20 +0000

    Add files via upload
    First commit

---
+++ Inicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alunos_mySQL
+{
+    public partial class Inicio : Form
+    {
+        private Login Login1;
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void PicBoxHelder_MouseHover(object sender, EventArgs e)
+        {
+            btnInicio.Visible = true;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login1 = new Login();
+            Login1.Show();
+        }
+    }
+}
